c52d5b311b68f38dce1f72b5db39f0edaef8766b
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2026-03-02 15:57:25 -0600

    Formatting

--- afolu/assets/small/__init__.py
+++ afolu/assets/small/__init__.py
@@ -2,9 +2,11 @@ import dagster as dg
 from afolu.assets.small import area_forecast, areas, transitions
 
 defs = dg.Definitions(
-    assets=dg.load_assets_from_modules([
-        areas,
-        # area_forecast,
-        transitions,
-    ]),
+    assets=dg.load_assets_from_modules(
+        [
+            areas,
+            # area_forecast,
+            transitions,
+        ],
+    ),
 )
